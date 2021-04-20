d3e589002070d081ef0cd4aa4078fc6f78ea0c86
DESKTOP-UHJ0L34\marki <marki.marketa@gmail.com>
2021-04-20 18:50:29 +0200

    update

--- 01-majitelka-vozu/app.js
+++ 01-majitelka-vozu/app.js
@@ -1,10 +1,31 @@
 // Vytvoř objekt auto, který bude mít vlastnosti: znacka, barva, rokVyroby,..
+let auto = {
+znacka: "Ford",
+barva: "černá",
+rokVyroby: "2010",
+spz: "1B7 K83"
+};
+
 
 
 // Přidej novou vlastnost (majitel a prirad ji svoje jmeno jako hodnotu)
+auto.majitel={};
+auto.majitel.jmeno="Markéta"
+auto.majitel.prijmeni="Musilovár"
+
 
 
 // Přepiš značku na Škoda
 
+auto["znacka"]= "Škoda";
+
 
 // Vytvor tri promenne majitelka, znacka a spz a vloz do techto elementu text z objektu auto.
+const majitelka= auto.majitel.jmeno + " " + auto.majitel.prijmeni;
+document.querySelector("#majitelka").innerHTML=majitelka;
+
+const auto = auto.znacka;
+document.querySelector("#znacka").innnerHTML=znacka;
+
+const spz = auto.spz;
+document.querySelector("#spz").innerHTML=spz;
